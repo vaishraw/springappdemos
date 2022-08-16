d2af8f917628dc3968a72cb015cd513eb42c7bbc
Vaishnavi <vaishnavirawat2902@gmail.com>
2022-08-16 13:12:12 +0530

    changes

---
+++ springcoredi/src/main/java/com/samples/springcoredi/App.java
@@ -0,0 +1,21 @@
+package com.samples.springcoredi;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext(
+				"com/samples/springcoredi/SpringConfig.xml");
+		//Employee emp = (Employee) springContainer.getBean("emp");
+		//System.out.println(emp);
+		
+		//Hospital hospital=(Hospital) springContainer.getBean("hospital");
+		//System.out.println(hospital);
+		
+		//CarDealer dealer=(CarDealer) springContainer.getBean("cardealer");
+		//System.out.println(dealer);
+		
+		Customer customer=(Customer) springContainer.getBean("customer");
+		System.out.println(customer);
+	}
+}
---
+++ springcoredi/src/main/java/com/samples/springcoredi/Hospital.java
@@ -0,0 +1,28 @@
+package com.samples.springcoredi;
+
+import java.util.List;
+
+public class Hospital {
+	
+	private String name;
+	private List<String> departments;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<String> getDepartments() {
+		return departments;
+	}
+	public void setDepartments(List<String> departments) {
+		this.departments = departments;
+	}
+	@Override
+	public String toString() {
+		return "Hospital [name=" + name + ", departments=" + departments + "]";
+	}
+	
+	
+
+}
